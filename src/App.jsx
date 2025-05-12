1c11d732805c2c55caebc4a3bc769025260cdf6e
contrerasalexa <akikoalexamma80@gmail.com>
2025-05-11 22:37:42 -0600

    Add Atletas and Nutriologos pages; update Dashboard with new routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Usuarios from "./pages/Usuarios";
 import MiHistorial from './pages/MiHistorial'
 import RegistrarAtleta from './pages/RegistrarAtleta';
 import RegistrarNutriologo from './pages/RegistrarNutriologo'
+import Nutriologos from './pages/Nutriologos';
+import Atletas from './pages/Atletas';
 /*import UserPanel from './pages/UserPanel'
 import PlanAlimenticio from './pages/PlanAlimenticio'
 import Hidratacion from './pages/Hidratacion'
@@ -20,6 +22,8 @@ export default function App() {
       <Route path="/mi-historial" element={<MiHistorial />} />
       <Route path="/registrar-atleta" element={<RegistrarAtleta />} />
       <Route path="/registrar-nutriologo" element={<RegistrarNutriologo />} />
+      <Route path="/nutriologos" element={<Nutriologos />} />
+      <Route path="/atletas" element={<Atletas />} />
       
     </Routes>
   )
